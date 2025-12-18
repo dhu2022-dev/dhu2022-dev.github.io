0219ab03ec1462788e670ce1ac120cb0addbcea9
David Hu <who.is.david101@gmail.com>
2025-12-18 16:30:46 -0500

    Revert adapter config (restore build)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from "astro/config";
-import github from "@astrojs/github";
 
 export default defineConfig({
   site: "https://dhu2022-dev.github.io",
-  adapter: github(),
 });
